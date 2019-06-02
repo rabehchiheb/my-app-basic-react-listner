095ff43fc2384bea931fc46ced3826fdada803eb
chiheb <chiheb.rabeh@gmail.com>
2019-06-02 15:39:38 +0100

    Updates

---
+++ precache-manifest.13cb59f30651ec85220af4a5378daae8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed3d1e3d91127708196c68c5ee9ddcee",
+    "url": "/my-app-basic-react-listner/index.html"
+  },
+  {
+    "revision": "b35be0121c62ad5caa35",
+    "url": "/my-app-basic-react-listner/static/css/main.51831703.chunk.css"
+  },
+  {
+    "revision": "5568663b56746e324730",
+    "url": "/my-app-basic-react-listner/static/js/2.1c62de68.chunk.js"
+  },
+  {
+    "revision": "b35be0121c62ad5caa35",
+    "url": "/my-app-basic-react-listner/static/js/main.4bed7c97.chunk.js"
+  },
+  {
+    "revision": "6bb2047ffe915eb024a3",
+    "url": "/my-app-basic-react-listner/static/js/runtime~main.4d386dab.js"
+  },
+  {
+    "revision": "4b4b18b004d5ea948462bd3c4b5912e4",
+    "url": "/my-app-basic-react-listner/static/media/card1.4b4b18b0.jpg"
+  },
+  {
+    "revision": "c033524b9c8985ae68aa243e16e99d99",
+    "url": "/my-app-basic-react-listner/static/media/card3.c033524b.jpg"
+  }
+]);
